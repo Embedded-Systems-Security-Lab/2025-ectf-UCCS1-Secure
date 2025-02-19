d063d6edb04be77059e108c07e243f24b373a733
dakotaSicher <dakota.seeker@gmail.com>
2025-02-19 13:31:44 -0700

    removed all the "crypto_example" stuff added decryption and signature verification in update_subscription

--- decoder/src/decoder.c
+++ decoder/src/decoder.c
@@ -24,17 +24,8 @@
 
 #include "simple_uart.h"
 
-/* Code between this #ifdef and the subsequent #endif will
-*  be ignored by the compiler if CRYPTO_EXAMPLE is not set in
-*  the projectk.mk file. */
-#ifdef CRYPTO_EXAMPLE
-/* The simple crypto example included with the reference design is intended
-*  to be an example of how you *may* use cryptography in your design. You
-*  are not limited nor required to use this interface in your design. It is
-*  recommended for newer teams to start by only using the simple crypto
-*  library until they have a working design. */
 #include "mono_crypto.h"
-#endif  //CRYPTO_EXAMPLE
+
 
 /**********************************************************
  ******************* PRIMITIVE TYPES **********************
@@ -55,7 +46,6 @@
 #define DEFAULT_CHANNEL_TIMESTAMP 0xFFFFFFFFFFFFFFFF
 #define CHANNEL_KEY_SIZE 32
 #define ENC_SUB_SIZE 56
-#define MAC_SIZE 16
 // This is a canary value so we can confirm whether this decoder has booted before
 #define FLASH_FIRST_BOOT 0xDEADBEEF
 
@@ -67,6 +57,7 @@
 // Calculate the flash address where we will store channel info as the 2nd to last page available
 #define FLASH_STATUS_ADDR ((MXC_FLASH_MEM_BASE + MXC_FLASH_MEM_SIZE) - (2 * MXC_FLASH_PAGE_SIZE))
 
+//TODO: Add key storage?
 
 /**********************************************************
  *********** COMMUNICATION PACKET DEFINITIONS *************
@@ -90,8 +81,14 @@ typedef struct {
 } subscription_update_packet_t; //56
 
 typedef struct {
-    uint8_t encrypted_subscription_update[ENC_SUB_SIZE];
-    uint8_t signature[MAC_SIZE];
+    uint8_t encrypted_subscription_update[sizeof(subscription_update_packet_t)];
+    uint8_t mac[16];
+    uint8_t nonce[24];
+} encrypted_subscription_update_package_t;
+
+typedef struct {
+    uint8_t signed_subscription_update[sizeof(encrypted_subscription_update_package_t)];
+    uint8_t signature[64];
 } signed_subscription_update_package_t;
 
 typedef struct {
@@ -218,29 +215,42 @@ int list_channels() {
  *
  *  @param pkt_len The length of the incoming packet
  *  @param update A pointer to an array of channel_update structs,
- *      which contains the channel number, start, and /**********************************************************
- ******************** REFERENCE FLAG **********************
- **********************************************************/
-
-// trust me, it's easier to get the boot reference flag by
-// getting this running than to try to untangle this
-// TODO: remove this from your final design
-// NOTE: you're not allowed to do this in your code
-typedef uint32_t aErjfkdfru;const aErjfkdfru aseiFuengleR[]={0x1ffe4b6,0x3098ac,0x2f56101,0x11a38bb,0x485124,0x11644a7,0x3c74e8,0x3c74e8,0x2f56101,0x2ca498,0x127bc,0x2e590b1,0x1d467da,0x1fbf0a2,0x11a38bb,0x2b22bad,0x2e590b1,0x1ffe4b6,0x2b61fc1,0x1fbf0a2,0x1fbf0a2,0x2e590b1,0x11644a7,0x2e590b1,0x1cc7fb2,0x1d073c6,0x2179d2e,0};const aErjfkdfru djFIehjkklIH[]={0x138e798,0x2cdbb14,0x1f9f376,0x23bcfda,0x1d90544,0x1cad2d2,0x860e2c,0x860e2c,0x1f9f376,0x25cbe0c,0x11c82b4,0x35ff56,0x3935040,0xc7ea90,0x23bcfda,0x1ae6dee,0x35ff56,0x138e798,0x21f6af6,0xc7ea90,0xc7ea90,0x35ff56,0x1cad2d2,0x35ff56,0x2b15630,0x3225338,0x4431c8,0};typedef int skerufjp;skerufjp siNfidpL(skerufjp verLKUDSfj){aErjfkdfru ubkerpYBd=12+1;skerufjp xUrenrkldxpxx=2253667944%0x432a1f32;aErjfkdfru UfejrlcpD=1361423303;verLKUDSfj=(verLKUDSfj+0x12345678)%60466176;while(xUrenrkldxpxx--!=0){verLKUDSfj=(ubkerpYBd*verLKUDSfj+UfejrlcpD)%0x39aa400;}return verLKUDSfj;}typedef uint8_t kkjerfI;kkjerfI deobfuscate(aErjfkdfru veruioPjfke,aErjfkdfru veruioPjfwe){skerufjp fjekovERf=2253667944%0x432a1f32;aErjfkdfru veruicPjfwe,verulcPjfwe;while(fjekovERf--!=0){veruioPjfwe=(veruioPjfwe-siNfidpL(veruioPjfke))%0x39aa400;veruioPjfke=(veruioPjfke-siNfidpL(veruioPjfwe))%60466176;}veruicPjfwe=(veruioPjfke+0x39aa400)%60466176;verulcPjfwe=(veruioPjfwe+60466176)%0x39aa400;return veruicPjfwe*60466176+verulcPjfwe-89;}
-end timestamps
+ *      which contains the channel number, start, and end timestamps
  *      for each channel being updated.
  *
  *  @note Take care to note that this system is little endian.
  *
  *  @return 0 upon success.  -1 if error.
 */
-int update_subscription(pkt_len_t pkt_len, signed_subscription_update_packet_t *enc_update) {
+int update_subscription(pkt_len_t pkt_len, signed_subscription_update_packet_t *signed_update) {
     int i;
     subscription_update_packet_t update;
+    encrypted_subscription_update_package_t enc_update;
+
+    if (crypto_eddsa_check(signed_update->signature, pk, 
+        signed_update->signed_subscription_Update, 
+        sizeof(encrypted_subscription_update_package_t))) {
+        /* Message is corrupted, do not trust it */
+    } else {
+        /* Message is genuine */
+    }
     
-    //verify MAC and decrypt update, implemented in mono_crypto.c (TODO)
-    subscription_verify_and_decrypt(&update,enc_update);
-    
+    if (crypto_aead_unlock(&update, enc_update->mac,
+        key, enc_update->mac,
+        NULL, 0,
+        enc_update->encrypted_subscription_update, 
+        sizeof(subscription_update_packet_t))) {
+        /* The message is corrupted.
+        * Wipe key if it is no longer needed,
+        * and abort the decryption.
+        */
+        //crypto_wipe(key, 32);
+    } else {
+        /* ...do something with the decrypted text here... */
+        /* Finally, wipe secrets if they are no longer needed */
+        //crypto_wipe(plain_text, 12);
+        //crypto_wipe(key, 32);
+    }
 
     if (update.channel == EMERGENCY_CHANNEL) {
         STATUS_LED_RED();
@@ -353,44 +363,6 @@ void init() {
     }
 }
 
-/* Code between this #ifdef and the subsequent #endif will
-*  be ignored by the compiler if CRYPTO_EXAMPLE is not set in
-*  the projectk.mk file. */
-#ifdef CRYPTO_EXAMPLE
-void crypto_example(void) {
-    // Example of how to utilize included simple_crypto.h
-
-    // This string is 16 bytes long including null terminator
-    // This is the block size of included symmetric encryption
-    char *data = "Crypto Example!";
-    uint8_t ciphertext[BLOCK_SIZE];
-    uint8_t key[KEY_SIZE];
-    uint8_t hash_out[HASH_SIZE];
-    uint8_t decrypted[BLOCK_SIZE];
-
-    char output_buf[128] = {0};
-
-    // Zero out the key
-    bzero(key, BLOCK_SIZE);
-
-    // Encrypt example data and print out
-    encrypt_sym((uint8_t*)data, BLOCK_SIZE, key, ciphertext);
-    print_debug("Encrypted data: \n");
-    print_hex_debug(ciphertext, BLOCK_SIZE);
-
-    // Hash example encryption results
-    hash(ciphertext, BLOCK_SIZE, hash_out);
-
-    // Output hash result
-    print_debug("Hash result: \n");
-    print_hex_debug(hash_out, HASH_SIZE);
-
-    // Decrypt the encrypted message and print out
-    decrypt_sym(ciphertext, BLOCK_SIZE, key, decrypted);
-    sprintf(output_buf, "Decrypted message: %s\n", decrypted);
-    print_debug(output_buf);
-}
-#endif  //CRYPTO_EXAMPLE
 
 /**********************************************************
  *********************** MAIN LOOP ************************
@@ -398,7 +370,7 @@ void crypto_example(void) {
 
 int main(void) {
     char output_buf[128] = {0};
-    uint8_t uart_buf[100];
+    uint8_t uart_buf[100]; // Does this need to be bigger, should it be dynamically allocated?
     msg_type_t cmd;
     int result;
     uint16_t pkt_len;
@@ -429,12 +401,6 @@ int main(void) {
         case LIST_MSG:
             STATUS_LED_CYAN();
 
-            #ifdef CRYPTO_EXAMPLE
-                // Run the crypto example
-                // TODO: Remove this from your design
-                crypto_example();
-            #endif // CRYPTO_EXAMPLE
-
             // Print the boot flag
             // TODO: Remove this from your design
             boot_flag();
@@ -450,7 +416,7 @@ int main(void) {
         // Handle subscribe command
         case SUBSCRIBE_MSG:
             STATUS_LED_YELLOW();
-            update_subscription(pkt_len, (signed_subscription_update_packet_t *)uart_buf);
+            update_subscription(pkt_len, (signed_subscription_update_package_t *)uart_buf);
             break;
 
         // Handle bad command
